cbaa6caa30b4095895a66c4337fc12e5934cb5d8
Jesse <jessemckinney513@gmail.com>
2021-05-07 16:17:28 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import styling from "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={styling.wrapper}>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={About} />
+            <Route path="/Home/" component={Home} />
+            <Route path="/Contact/" component={Contact} />
+            <Route path="*" component={Error} />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
