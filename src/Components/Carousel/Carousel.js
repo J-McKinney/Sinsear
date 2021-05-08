ce8b663530aa239f2dcf7c26b7e3b3f79fc92d50
Jesse <jessemckinney513@gmail.com>
2021-05-08 14:22:26 -0400

    yay

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import drinks from "../../drinks.json";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
@@ -8,21 +7,21 @@ import "swiper/components/effect-fade/effect-fade.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
 // import Swiper core and required modules
-// import SwiperCore, { EffectFade, Navigation, Pagination } from "swiper/core";
+import SwiperCore, { EffectFade, Navigation, Pagination } from "swiper/core";
 // My Styling
+import drinks from "../../drinks.json";
 import styling from "./Carousel.module.css";
+
+// install Swiper modules
+SwiperCore.use([EffectFade, Navigation, Pagination]);
 
 class Carousel extends Component {
   state = {
     drinks,
   };
 
-  componentDidMount() {
-    console.log("Drinks:");
-    console.log(this.state.drinks);
-    console.log(this.state.drinks[0]);
-    console.log(this.state.drinks[0].image);
-  }
+  componentDidMount() {}
+  componentDidUpdate() {}
 
   render() {
     return (
@@ -37,12 +36,17 @@ class Carousel extends Component {
               clickable: true,
             }}
           >
-            <SwiperSlide>
-              <img alt="Aviation" src={this.state.drinks[0].image} />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img alt="Deoch Te" src={this.state.drinks[1].image} />
-            </SwiperSlide>
+            {this.state.drinks.map((drink) => (
+              <SwiperSlide>
+                <img
+                  alt=""
+                  className={styling.img}
+                  id={drink.id}
+                  key={drink.id}
+                  src={drink.image}
+                />
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </>
